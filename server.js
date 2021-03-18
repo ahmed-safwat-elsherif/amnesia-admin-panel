7a2a7597f8fd4fa611bb7f67efede1fb546d6356
unknown <s-aelsherif500@zewailcity.edu.eg>
2021-03-18 22:11:25 +0200

    change name to e-commerce

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/admin-panel'));
+app.use(express.static('./dist/e-commerce'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/admin-panel/'}),
+    res.sendFile('index.html', {root: 'dist/e-commerce/'}),
 );
 
 // Start the app by listening on the default Heroku port
